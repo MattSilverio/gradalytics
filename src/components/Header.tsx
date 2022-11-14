9403d58a032490abaeb3ed8d3bf3b1ebd3d8ee45
Breno Romeiro <bsromeiro@gmail.com>
2022-11-14 08:38:46 -0300

    wip

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,13 @@ import React from "react";
 
 type HeaderType = {
   children?: React.ReactNode;
+  className?: string;
 };
 
-const commonClassName = "";
-
-export const H1: React.FC<HeaderType> = ({ children }) => (
-  <h1 className="text-6xl font-bold">{children}</h1>
+export const H1: React.FC<HeaderType> = ({ children, className = "" }) => (
+  <h1 className={`text-7xl font-bold ${className}`}>{children}</h1>
 );
 
 export const H2: React.FC<HeaderType> = ({ children }) => (
-  <h1 className="text-4xl font-bold">{children}</h1>
+  <h1 className="text-3xl font-bold">{children}</h1>
 );
--- src/pages/student-details/StudentDetails.jsx
+++ src/pages/student-details/StudentDetails.jsx
@@ -1,8 +1,35 @@
-export function StudentDetails(){
+import { Fragment } from "react";
+import { H1, H2 } from "../../components/Header";
+import { Paragraph } from "../../components/Paragraph";
 
-    return(
-        <>
-            <h1>Aqui é StudentDetails</h1>
-        </>
-    )
+export function StudentDetails() {
+  return (
+    <div className="m-10">
+      <H2>Resultado</H2>
+      <div className="w-fit flex flex-col items-center justify-center my-28 mx-auto">
+        <div>
+          <H1 className="text-8xl">80%</H1>
+        </div>
+        <span className="ml-auto font-bold text-xl">8/10</span>
+      </div>
+
+      <Paragraph className="font-bold text-xl">💡 Sugestões</Paragraph>
+      <ul className="[&>*]:ml-6">
+        <li className="mt-4">
+          Estude isso aqui (
+          <a className="text-blue-500" href="#">
+            LINK
+          </a>
+          ) para um melhor desempenho em #Algoritmos
+        </li>
+        <li className="mt-4">
+          Estude isso aqui (
+          <a className="text-blue-500" href="#">
+            LINK
+          </a>
+          ) para um melhor desempenho em #Cálculo
+        </li>
+      </ul>
+    </div>
+  );
 }
